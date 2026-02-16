37f837b7be2a0b1b5d7cc558c24fbfe4244dd128
Reyhan Y. Silalahi <reyhanyosafatsilalahi@gmail.com>
2026-02-16 10:56:28 +0700

    UI Automation with Mocha Hooks and explicit wait

--- test/sauce.test.js
+++ test/sauce.test.js
@@ -1,21 +1,27 @@
 const { Builder, By, until } = require("selenium-webdriver");
 const assert = require("assert");
 
-describe("SauceDemo Automation Test", function () {
+describe("SauceDemo Automation Test - With Hooks", function () {
   this.timeout(30000);
+
   let driver;
 
+  // 🔹 Run 1x sebelum semua test
   before(async function () {
     driver = await new Builder().forBrowser("chrome").build();
   });
 
+  // 🔹 Run sebelum setiap test
+  beforeEach(async function () {
+    await driver.get("https://www.saucedemo.com/");
+  });
+
+  // 🔹 Run 1x setelah semua test
   after(async function () {
     await driver.quit();
   });
 
   it("Sukses Login", async function () {
-    await driver.get("https://www.saucedemo.com/");
-
     await driver.findElement(By.id("user-name"))
       .sendKeys("standard_user");
 
@@ -29,22 +35,37 @@ describe("SauceDemo Automation Test", function () {
   });
 
   it("Urutkan Produk dari A-Z", async function () {
-    const sortDropdown = await driver.findElement(
-        By.className("product_sort_container")
+    // 🔹 LOGIN
+    await driver.findElement(By.id("user-name"))
+      .sendKeys("standard_user");
+
+    await driver.findElement(By.id("password"))
+      .sendKeys("secret_sauce");
+
+    await driver.findElement(By.id("login-button")).click();
+
+    // 🔹 Masuk halaman Products
+    await driver.wait(
+      until.elementLocated(By.className("title")),
+      5000
     );
 
-    // Pilih sorting A-Z (value = az)
-    await sortDropdown.findElement(By.css('option[value="az"]')).click();
+    const pageTitle = await driver.findElement(By.className("title"));
+    await driver.wait(until.elementIsVisible(pageTitle), 5000);
 
-    await driver.wait(async () => {
-        const items = await driver.findElements(By.className("inventory_item_name"));
-        const text = await items[0].getText();
-        return text === "Sauce Labs Backpack";
-    }, 5000);
+    // 🔹 Cari dropdown sorting
+    const sortDropdown = await driver.wait(
+      until.elementLocated(By.className("product_sort_container")),
+      5000
+    );
+
+    await driver.wait(until.elementIsVisible(sortDropdown), 5000);
+
+    await sortDropdown.findElement(By.css('option[value="az"]')).click();
 
     const products = await driver.findElements(By.className("inventory_item_name"));
     const firstProduct = await products[0].getText();
 
     assert.strictEqual(firstProduct, "Sauce Labs Backpack");
-});
+  });
 });
